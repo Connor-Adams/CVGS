4e949e4783fd13cba0d378b762ab0c6416a0ee3c
Joshua Arnott <arnottjoshua@gmail.com>
2021-11-29 18:14:01 -0500

    changed the home page

--- Team6CVGS/Controllers/HomeController.cs
+++ Team6CVGS/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -14,20 +15,31 @@ namespace Team6CVGS.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CVGSContext _context;
         private readonly ILogger<HomeController> _logger;
         private RoleManager<IdentityRole> roleManager;
 
-        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleMgr)
+        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleMgr, CVGSContext context)
         {
             roleManager = roleMgr;
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            int total = _context.Games.Count();
+            Random r = new Random();
+            int offset = r.Next(0, total);
 
-            
-            return View();
+            var cVGSContext = _context.Games.Skip(offset).FirstOrDefaultAsync();//.Include(g => g.EsrbRatingCodeNavigation).Include(g => g.GameCategory).Include(g => g.GamePerspectiveCodeNavigation).Include(g => g.GameStatusCodeNavigation).Include(g => g.GameSubCategory).Include(g => g.Guid);
+            string trailer = "https://www.youtube.com/embed/mkHJzHIbQQg"; // Probably shouldn't be hardcoded, but they're all the same anyway and this was faster :)
+
+            // Extract the youtube video id from the trailer column
+
+            ViewData["trailer"] = trailer;
+
+            return View(await cVGSContext);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -36,6 +48,6 @@ namespace Team6CVGS.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        
+
     }
 }
